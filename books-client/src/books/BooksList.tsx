d5bd76b60d6f88f92854165c4c504d2b955fe3c3
Ivan Milinkovic <ivanmcode@gmail.com>
2025-04-27 18:06:32 +0200

    Cleanup

--- books-client/src/books/BooksList.tsx
+++ books-client/src/books/BooksList.tsx
@@ -18,7 +18,7 @@ export const BooksList = ({ books, handleLastElementVisible }: Props) => {
       });
       if (node) observer.current.observe(node);
     },
-    [books, handleLastElementVisible]
+    [handleLastElementVisible]
   );
 
   return (
